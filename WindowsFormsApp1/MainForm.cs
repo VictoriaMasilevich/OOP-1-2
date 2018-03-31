e755a3b885aa14a61b97ed31048537d31c5ebb5e
Victoria Masilevich <tori_masya@mail.ru>
2018-03-31 21:35:41 +0300

    Customizing tools

---
+++ WindowsFormsApp1/MainForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Geometry
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+            MainForm_Window();
+        }
+
+        private void MainForm_Window()
+        {
+            PictureBox pct = new PictureBox();
+            pct.Height = 300;
+            pct.Width = 600;
+            pct.BackColor = Color.White;
+            Controls.Add(pct);
+        }
+
+    }
+}
